7f4e82c9e76987306387417db6af61e1dbaf4686
guanquann <tanguanquan@yahoo.com>
2022-07-14 21:14:15 +0800

    feature: discord integration

--- api/temp_linkedin.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-def get_linkedin_followers():
-    url = 'https://www.linkedin.com/pages-extensions/FollowCompany?id=76491268&counter=bottom'
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    followers = soup.select_one('.follower-count').text
-    print(followers)
